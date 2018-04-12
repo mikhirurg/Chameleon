01dee8ae5c4629ddd90c724a9a85136cce4123e6
InviewCompany <mr_a953@mail.ru>
2018-04-12 19:16:11 +0300

    До дедлайна 3 дня

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -1,19 +1,29 @@
+
+
 import java.util.Scanner;
 
 import static java.lang.System.out;
 
 public class Encrypt {
     Encrypt(String user){
-        out.println("Здесь будет шифровальщик");
         Scanner scn=new Scanner(System.in);
         boolean isStart=true;
         while(isStart) {
-            String commandEnc=scn.nextLine();
-            switch (commandEnc){
+            out.println(user+">");
+            String commandDec=scn.nextLine();
+            switch (commandDec){
+                case "/exit":
+                    new Exit();
+                    break;
 
                 case "/stop":
+                    out.println("Конец работы шифровальщика");
                     isStart=false;
                     break;
+
+
+                default:
+                    new Error(commandDec);
             }
         }
     }
